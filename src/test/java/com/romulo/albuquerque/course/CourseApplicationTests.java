05f7b4f9597651b93d9ad6b127bda1216c39f8ae
Romulo Albuquerque <romulo.albuquerque@live.com>
2021-08-13 13:59:02 -0300

    Project created

---
+++ src/test/java/com/romulo/albuquerque/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romulo.albuquerque.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
